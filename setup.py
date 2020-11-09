bbeed932296f78eb608be02d243e2191ed5840ac
Philipp Gualdi <philipp.gualdi@tugraz.at>
2020-11-09 14:58:25 +0100

    test

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ tests_require = [
 
 # Should follow inveniosoftware/invenio versions
 invenio_db_version = ">=1.0.4,<2.0.0"
-invenio_search_version = ">=1.4.0,<2.0.0"
+invenio_search_version = ">=1.0.0,<2.0.0"
 
 extras_require = {
     'docs': [

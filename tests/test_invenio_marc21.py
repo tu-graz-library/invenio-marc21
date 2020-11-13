e82b7a938cc976ab2795edcffea85f846c92d23a
Philipp Gualdi <philipp.gualdi@tugraz.at>
2020-11-13 12:54:43 +0100

    refactoring test files
    update import order and codestyle

--- tests/test_invenio_marc21.py
+++ tests/test_invenio_marc21.py
@@ -2,6 +2,7 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2016-2018 CERN.
+# Copyright (C) 2020 Graz University of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -10,9 +11,10 @@
 """Module tests."""
 
 from __future__ import absolute_import, print_function
-import pytest
+
 import mock
 import pkg_resources
+import pytest
 from dojson.contrib.marc21 import marc21
 from dojson.contrib.marc21.utils import load
 from flask import Flask
@@ -26,20 +28,21 @@ from invenio_marc21 import InvenioMARC21
 def test_version():
     """Test version import."""
     from invenio_marc21 import __version__
+
     assert __version__
 
 
 def test_init():
     """Test extension initialization."""
-    app = Flask('testapp')
+    app = Flask("testapp")
     ext = InvenioMARC21(app)
-    assert 'invenio-marc21' in app.extensions
+    assert "invenio-marc21" in app.extensions
 
-    app = Flask('testapp')
+    app = Flask("testapp")
     ext = InvenioMARC21()
-    assert 'invenio-marc21' not in app.extensions
+    assert "invenio-marc21" not in app.extensions
     ext.init_app(app)
-    assert 'invenio-marc21' in app.extensions
+    assert "invenio-marc21" in app.extensions
 
 
 def mock_record_validate(self):
@@ -47,18 +50,17 @@ def mock_record_validate(self):
     pass
 
 
-@mock.patch('invenio_records.api.Record.validate', mock_record_validate)
+@mock.patch("invenio_records.api.Record.validate", mock_record_validate)
 def load_records(es_app, filename, schema):
     """Try to index records."""
     indexer = RecordIndexer()
     with es_app.test_request_context():
-        data_filename = pkg_resources.resource_filename(
-            'invenio_records', filename)
+        data_filename = pkg_resources.resource_filename("invenio_records", filename)
         records_data = load(data_filename)
         records = []
         for item in records_data:
             item_dict = dict(marc21.do(item))
-            item_dict['$schema'] = schema
+            item_dict["$schema"] = schema
             record = Record.create(item_dict)
             records.append(record)
         db.session.commit()
@@ -68,25 +70,22 @@ def load_records(es_app, filename, schema):
             es_records.append(indexer.index(record))
 
         from invenio_search import current_search
+
         for record in es_records:
-            current_search.client.get(index=record['_index'],
-                                      doc_type=record['_type'],
-                                      id=record['_id'])
+            current_search.client.get(
+                index=record["_index"], doc_type=record["_type"], id=record["_id"]
+            )
 
 
 @pytest.mark.skip(reason="no way of currently testing this")
 def test_authority_data(es_app, request):
     """Test indexation using authority data."""
-    schema = ('http://localhost:5000/'
-              'marc21/authority/ad-v1.0.0.json')
-    load_records(es_app=es_app, filename='data/marc21/authority.xml',
-                 schema=schema)
+    schema = "http://localhost:5000/" "marc21/authority/ad-v1.0.0.json"
+    load_records(es_app=es_app, filename="data/marc21/authority.xml", schema=schema)
 
 
 @pytest.mark.skip(reason="no way of currently testing this")
 def test_bibliographic_data(es_app, request):
     """Test indexation using bibliographic data."""
-    schema = ('http://localhost:5000/'
-              'marc21/bibliographic/bd-v1.0.0.json')
-    load_records(es_app=es_app, filename='data/marc21/bibliographic.xml',
-                 schema=schema)
+    schema = "http://localhost:5000/" "marc21/bibliographic/bd-v1.0.0.json"
+    load_records(es_app=es_app, filename="data/marc21/bibliographic.xml", schema=schema)

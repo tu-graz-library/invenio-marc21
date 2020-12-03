13a178bb6706728cfc211b6f9767749c80d8f7b1
Philipp Gualdi <philipp.gualdi@tugraz.at>
2020-12-03 13:03:39 +0100

    bugfix(rest-api): make it work with modern api
    changing the rest config of marc21 module to work with newer invenio instance
    and init config in records_rest_endpoints. Updating setup requirements using
    newer versions and adding marc21 module to the invenio_base.api_apps and
    invenio_config.

--- invenio_marc21/ext.py
+++ invenio_marc21/ext.py
@@ -2,6 +2,7 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2016-2018 CERN.
+# Copyright (C) 2020 Graz University of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -9,6 +10,8 @@
 """Invenio module with nice defaults for MARC21 overlay."""
 
 from __future__ import absolute_import, print_function
+
+from werkzeug.utils import cached_property
 
 from . import config
 from .views import blueprint
@@ -32,7 +35,7 @@ class InvenioMARC21(object):
         """
         self.init_config(app)
         app.register_blueprint(blueprint)
-        app.extensions['invenio-marc21'] = self
+        app.extensions["invenio-marc21"] = self
 
     def init_config(self, app):
         """Initialize configuration.
@@ -40,9 +43,14 @@ class InvenioMARC21(object):
         :param app: An instance of :class:`flask.Flask`.
         """
         app.config.setdefault(
-            'MARC21_BASE_TEMPLATE',
-            app.config.get('BASE_TEMPLATE',
-                           'invenio_marc21/base.html'))
+            "MARC21_BASE_TEMPLATE",
+            app.config.get("BASE_TEMPLATE", "invenio_marc21/base.html"),
+        )
         for k in dir(config):
-            if k.startswith('MARC21_'):
+            if k.startswith("MARC21_"):
+                if k == "MARC21_REST_ENDPOINTS":
+                    # Make sure of registration process.
+                    app.config.setdefault("RECORDS_REST_ENDPOINTS", {})
+                    app.config["RECORDS_REST_ENDPOINTS"].update(getattr(config, k))
+
                 app.config.setdefault(k, getattr(config, k))
